359ff4fe3d0bd5025135848f9b974bdf19c41d59
YooSeop-Shin <shin5179@naver.com>
2023-01-11 16:31:21 +0900

    snowman v1

--- sf.php
+++ sf.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>-Sol Flix V-SF,공상과학-</title>
+        <title>SF,공상과학-Virginia</title>
         <!-- Favicon-->     
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
